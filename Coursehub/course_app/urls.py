67e296d5342a0020423fdf901b8d2450fc53b8ce
janki-parmar <jankiparmarofficial@gmail.com>
2025-02-05 17:27:11 +0530

    dashboards

--- Coursehub/course_app/urls.py
+++ Coursehub/course_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.login, name='login'),
     path('registration', views.registration, name= 'registration'),
+    path('dashboard/admin', views.admin_dashboard, name= 'admin_dashboard'),
+    path('dashboard/instructor', views.instructor_dashboard, name= 'instructor_dashboard'),
+    path('dashboard/student', views.student_dashboard, name= 'student_dashboard'),
 ]
--- Coursehub/course_app/views.py
+++ Coursehub/course_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import *
+from django.contrib.auth.hashers import make_password, check_password
 import logging
 logger = logging.getLogger(__name__)
 
@@ -16,7 +17,8 @@ def registration(request):
             lastname = request.POST['lastname']
             role = request.POST['role']
 
-            user = UserProfile.objects.create(username = username, email = email, password = password, firstname = firstname, lastname = lastname, role = role)
+            hash_password = make_password(password)
+            user = UserProfile.objects.create(username = username, email = email, password = hash_password, firstname = firstname, lastname = lastname, role = role)
 
             return redirect('login')  
         
@@ -29,19 +31,43 @@ def registration(request):
 def login(request):
     if request.method == 'POST':
 
-        username = request.POST.get('username')
-        email = request.POST.get('email')
+        username_email = request.POST.get('username_email')
         password = request.POST.get('password')
+
     
         try :   
-                user = UserProfile.objects.get(username = username) if username else UserProfile.objects.get(email=email)
+                if '@' in username_email:
+                    user = UserProfile.objects.get(email = username_email)
+                    print("=------------emiallll")
+                else:
+                    user = UserProfile.objects.get(username = username_email)
+                    print("=------------name")
 
-                if user and user.password == password:
-                    return redirect('dashboard')  
+                if check_password(password, user.password):
+                    if user.role == 'admin':
+                        return redirect('admin_dashboard') 
+                    elif user.role == 'instructor':
+                        return redirect('instructor_dashboard')
+                    elif user.role == 'student':
+                        return redirect('student_dashboard')
+                    else:
+                        return render(request, 'login.html', {'error': 'Role not defined'})
+                   
                 else:
                     return render(request, 'login.html', {'error': 'Invalid credentials'})
         except Exception as e:
             return render(request, 'login.html', {'error': str(e)})
 
     return render(request, 'login.html')
+
+
+def admin_dashboard(request):
+    return render(request, 'admin_dashboard.html')
+
+def instructor_dashboard(request):
+    return render(request, 'instructor_dashboard.html')
+
+def student_dashboard(request):
+    return render(request,'student_dashboard.html')
                 
+
